f14614e4d78059edb1ee7b3b176535e65f889fdf
jennasaikaly <jen.saikaly@gmail.com>
2022-04-03 15:18:07 -0400

    added local storage finally

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,7 @@
-console.log(moment().format());
 
-//$(document).ready(function() {});
+
+
+
 //Today's date displayed at the top of the page
 let currentDate = moment().format("MMMM Do, YYYY - hh:mm:ss a");
 $("#currentDate").text(currentDate);
@@ -10,13 +11,42 @@ let currentTime = moment().format("HH:mm");
 console.log(currentTime);
 
 
+//load task function
+let loadTasks = function (){
+tasks = JSON.parse(localStorage.getItem("tasks"));
+
+//if there is nothing in localStorage, create a new object to task the tasks
+if (!tasks) {
+  tasks = {};
+  };
+
+  for(let i=0; i<localStorage.length; i++) {
+    let tasks = localStorage.tasks(i);
+    alert(`${tasks}: ${localStorage.getItem(tasks)}`);
+    console.log(tasks)
+  }
+  displayTasks(tasks);
+
+}
+
+//displayTasks(tasks){
+
+ // $.each(tasks)
+//}
+
+
+//Saves the task description and time when the user clicks the 'save' button
+$(".saveBtn").click(function(event) {
+  event.preventDefault();
+  console.log("save button was clicked");
+//get form values
+ var taskText= $(this).siblings(".description").val();
+ var taskTime= $(this).parent().attr("id");
+
+ localStorage.setItem(taskText, taskTime);
+
  
+  
+  
+})  
 
-
-//Saves the task description and 
-$(".saveBtn").on("click", function() {
-  let text = $(this).siblings(".description").val();
-  let time = $(this).closest(".time-div").attr("id");
-
-  localStorage.setItem(text, time);
-})
